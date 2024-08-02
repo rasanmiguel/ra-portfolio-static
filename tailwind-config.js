926e45c4c2a24a843cdd2b0281dbc5662e0f5a19
rasm <ralphadriansanmiguel@gmail.com>
2024-08-02 21:52:47 +0800

    Add tailwind configuration and start migration from vanilla css to tailwind css

---
+++ tailwind-config.js
@@ -0,0 +1,14 @@
+// Tailwind Config JS
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+            },
+            fontFamily: {
+                josefin: ["Josefin Sans", 'sans-serif'],
+                boldfont: ["The Bold Font", 'sans-serif']
+            },
+        },
+    },
+}
