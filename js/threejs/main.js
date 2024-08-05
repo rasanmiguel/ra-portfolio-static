e89969135578e4f9a5ed9ea475954995f97b7dcf
rasm <ralphadriansanmiguel@gmail.com>
2024-08-06 00:43:07 +0800

    Implement renderer, animation, and model loader code for 3d scene

--- js/threejs/main.js
+++ js/threejs/main.js
@@ -8,9 +8,33 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-// Model
+// Model Loader
+const loader = new GLTFLoader();
+let model;
+
+loader.load('./assets/3d/burger-textured.glb',
+    (gltf) => {
+        model = gltf.scene;
+        scene.add(model);
+    }, undefined,
+    (error) => {
+        console.error(error);
+    }
+);
 
 // Renderer
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // Animation
+const animate = () => {
+    requestAnimationFrame(animate);
+    if (model) {
+        model.rotation.y += 0.01;
+    }
+    renderer.render(scene, camera);
+};
 
+// Start Animation
+animate();
