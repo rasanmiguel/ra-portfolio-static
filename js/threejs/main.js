657600075ef66da1a7d7e8fc3e8605c518c9cffe
rasm <ralphadriansanmiguel@gmail.com>
2024-08-06 00:32:49 +0800

    Refactored main.js, added ThreeJS scene and ThreeJS camera

--- js/threejs/main.js
+++ js/threejs/main.js
@@ -1,3 +1,16 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
+// Scene
+const scene = new THREE.Scene();
+
+// Camera
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Model
+
+// Renderer
+
+// Animation
+
