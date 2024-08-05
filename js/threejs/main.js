910e1b15a3b954a98971ce567eed9b8860bf9184
rasm <ralphadriansanmiguel@gmail.com>
2024-08-06 04:05:33 +0800

    Finalize styling for Skills Section, preparing markup for Projects section

--- js/threejs/main.js
+++ js/threejs/main.js
@@ -93,3 +93,4 @@ window.addEventListener('resize', updateSize);
 updateSize(); // Initial size update
 
 
+
--- tailwind-config.js
+++ tailwind-config.js
@@ -5,7 +5,13 @@ tailwind.config = {
             colors: {
                 clifford: '#da373d',
                 yellow: 'yellow',
-                tangerine: '#f5630b'
+                // My shades of green
+                tangerine: '#f5630b',
+                celadon: '#afe1af',
+                eucalyptus: '#5f8575',
+                grass: '#7cfc00',
+                jade: '#00a36c',
+                lightgreen: '#90ee90'
             },
             fontFamily: {
                 josefin: ["Josefin Sans", 'sans-serif'],
